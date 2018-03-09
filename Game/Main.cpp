b480355f959f643cd5fb8a4628bb97353fe218e9
KimDeokwon <64bit_decryption@naver.com>
2018-03-09 12:03:51 +0900

    Program entry point

---
+++ Game/Main.cpp
@@ -0,0 +1,12 @@
+﻿#include <Core.h>
+#include "Test.h"
+
+using namespace PradleEngine;
+
+int main(void)
+{
+	Engine* engine = Engine::Create(CODE_PAGE::KOREAN, 30);
+	engine->Init();
+	engine->Excute(new Test);
+	return 1;
+}
